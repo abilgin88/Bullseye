c2f1b182edec7a8c6082175f94bc8dbbfdcc8aff
Abdullah Bilgin <abilgin02@icloud.com>
2023-04-08 14:46:29 +0300

    Test-Driven Development to test the Game’s points method

--- BullsEye/BullsEyeTests/BullsEyeTests.swift
+++ BullsEye/BullsEyeTests/BullsEyeTests.swift
@@ -14,20 +14,31 @@ final class BullsEyeTests: XCTestCase {
   // create an instance of our game struct.
   var game: Game!
   
-    override func setUpWithError() throws {
-      // set this game property to an instance of our game
-      game = Game()
-    }
-
-    override func tearDownWithError() throws {
-      // get rid of the value of the game property
-      game = nil
-    }
-
-    func testExample() throws {
-      // call XCTAssertEqual. This is saying these two values that you pass in should be the same.
-      XCTAssertEqual(game.point(sliderValue: 50), 999)
-    }
-
-
+  override func setUpWithError() throws {
+    // set this game property to an instance of our game
+    game = Game()
+  }
+  
+  override func tearDownWithError() throws {
+    // get rid of the value of the game property
+    game = nil
+  }
+  
+  // add one where the slider value is greater than the target value.
+  func testScorePositive() {
+    var guess = game.target + 5
+    var score = game.point(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
+  
+  // add a second test where the slider value is less than the target value
+  func testScoreNegative() {
+    var guess = game.target - 5
+    var score = game.point(sliderValue: guess)
+    XCTAssertEqual(score, 95)
+  }
+  
+  
+  
+  
 }
