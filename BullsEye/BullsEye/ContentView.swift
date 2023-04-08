f54409943a45124f8eefe5da6e7fdea7337c8e61
Abdullah Bilgin <abilgin02@icloud.com>
2023-04-08 09:50:28 +0300

    Style the text like Luke’s design

--- BullsEye/BullsEye/ContentView.swift
+++ BullsEye/BullsEye/ContentView.swift
@@ -11,10 +11,21 @@ struct ContentView: View {
   var body: some View {
     VStack {
       Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        .bold()
+        .multilineTextAlignment(.center)
+        .lineSpacing(4.0)
+        .font(.footnote)
+        .kerning(2.0)
       Text("89")
+        .kerning(-1.0)
+        .font(.largeTitle)
+        .fontWeight(.black)
       HStack {
         Text("1")
+          .bold()
         Slider(value: .constant(50), in: 1.0 ... 100.0)
+        Text("100")
+          .bold()
       }
       Button("Hit Me") {
         
