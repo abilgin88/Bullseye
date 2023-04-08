162966525a4728958e1b0eba82cdf424711eb887
Abdullah Bilgin <abilgin02@icloud.com>
2023-04-08 15:06:41 +0300

    implement the algorithm we just designed to calculate the difference between the slider and the target value

--- BullsEye/BullsEye/Models/Game.swift
+++ BullsEye/BullsEye/Models/Game.swift
@@ -8,12 +8,26 @@
 import Foundation
 //create a new game struct
 struct Game {
-  var target: Int = 45
+  var target: Int = Int.random(in: 1...100)
   var score: Int = 0
   var round: Int = 1
   
   //  Create a method for calculating points
   func point(sliderValue: Int) -> Int {
-    return 999
+    // create a local variable inside this method called the difference
+    var difference: Int
+    
+    // if-else based on our plain-english algorithm
+    if sliderValue > target {
+      difference = sliderValue - target
+    } else if target > sliderValue {
+      difference = target - sliderValue
+    } else {
+      difference = 0
+    }
+    
+    // create a local variable inside this method called the awardedPoints
+    var awardedPoints: Int = 100 - difference
+    return awardedPoints
   }
 }
