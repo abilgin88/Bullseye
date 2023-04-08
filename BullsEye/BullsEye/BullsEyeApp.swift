8ba85e0ffef54aea07aa04b14c546fe3d5b6c456
Abdullah Bilgin <abilgin02@icloud.com>
2023-04-08 05:26:47 +0300

    Creating project file

---
+++ BullsEye/BullsEye/BullsEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullsEyeApp.swift
+//  BullsEye
+//
+//  Created by Abdullah Bilgin on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct BullsEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
